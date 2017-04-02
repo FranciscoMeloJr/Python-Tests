51baeb4726a799caa2edbff17b17f0241f2dee75
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2017-04-02 02:16:31 -0400

    Class metric changes for new metric creation

--- Classification/metric.py
+++ Classification/metric.py
@@ -1,21 +1,43 @@
 
-#Class used to control each metric
-class Metric(object):
+from execution import Execution
+
+"This function will create the runs for testing"
+def create_list_Executions(tam):
+    list_objects = []
+    i = 0
+    each = [1,1,0]
+    while (i < tam):
+        x = Execution(i, each)
+        list_objects.append(x)
+        i = i + 1
+
+    y = [0, 0, 1]
+    x = Execution(i, y)
+    list_objects.append(x)
+    return list_objects
+
+"Class to keep the groups"
+class Group(object):
     """A simple example class"""
     id = -1
     executions_ids = []
-    name = ""
+    group_name = ""
+    metric_name = ""
 
     def __init__(self, id):
         self.id = id
 
-    def __init__(self, id, name= "unknown", exe_ids = []):
+    def __init__(self, id, group_name, metric_name= "unknown", exe_ids = []):
         self.id = id
-        self.name = name
+        self.metric_name = metric_name
+        self.group_name = group_name
         self.executions_ids = exe_ids
 
+    def add_execution(self, execution):
+        self.executions_ids = execution
+
     def show(self):
-        print (self.executions_ids)
+        print (self.group_name)
         return
 
     def set_executions(self, executions):
@@ -33,5 +55,137 @@ class Metric(object):
     def get_name(self):
         return self.name
 
+"Class metric"
+class Metric(object):
+    groups = []
+    name = ""
+    list_executions = []
 
-x = Metric(27, "Velocidade")
+    def __init__(self, name, groups= [], id = -1):
+        self.id = id
+        self.name = name
+        if len(groups) > 0:
+            self.create_groups(groups)
+        else:
+            self.groups = groups
+
+    def set_groups(self, groups):
+        self.groups = groups
+
+    def show(self):
+        print self.groups
+
+    def qtd_groups(self):
+        return len(self.groups)
+
+    def set_list_execution(self, list):
+        self.list_executions = list
+
+    def get_executions_groups(self):
+        return self.list_executions
+
+    "This function to creates the groups inside the metric"
+    def create_groups(self, list_values):
+        list_values = sorted(list_values)
+        list_groups = []
+        aux = []
+        previous = list_values[0]
+
+        for each in list_values:
+            if each == previous:
+                aux.append(each)
+            else:
+                list_groups.append(aux)
+                aux = []
+                aux.append(each)
+            previous = each
+        list_groups.append(aux)
+        self.groups = list_groups
+
+    "Creates a metric from an execution list"
+    def create_from_execution_list(self, list_executions, index):
+        values = []
+        matrix = []
+        temp = []
+        for each in list_executions:
+            v = each.get_index_metrics(index)
+            id = each.get_id()
+            values.append(v)
+            temp = [v, id]
+            matrix.append(temp)
+
+        self.create_groups(values)
+
+        print matrix
+
+        temp = []
+        result_matrix = []
+        for each in matrix:
+            group = self.give_value_I_will_find_group(each[0])#value
+            each[1]#id
+            temp = [each[1], each[0], group]
+            result_matrix.append(temp)
+
+        self.list_executions = result_matrix
+
+    "This function to find the group of a variable"
+    def give_value_I_will_find_group(self, value):
+
+        i = 0
+        for eachG in self.groups:
+            for eachValue in eachG:
+                if eachValue == value:
+                    return i
+            i = i + 1
+        return -1
+
+"This function to create a test to create executions"
+def test_create_executions():
+    list_executions = create_list_Executions(10)
+
+    x = Group(27, "Group 0", "Speed")
+    z = Group(28, "Group 1", "Speed")
+
+    print z.get_executions()
+    print x.get_executions()
+
+    group_g1 = []
+    group_g0 = []
+    for each in list_executions:
+        group_choice = each.get_index_metrics(0)
+        g0 = 0
+        g1 = 1
+
+        if (group_choice == g0):
+            print "g0"
+            group_g0.append(each.get_id())
+        if (group_choice == g1):
+            print "g1"
+            group_g1.append(each.get_id())
+    z.add_execution(group_g0)
+    x.add_execution(group_g1)
+
+    # groups:
+    print z.get_executions()
+    print x.get_executions()
+
+    return [x,z]
+
+"Main function"
+def main():
+    list = create_list_Executions(10)
+
+    # Metrics:
+    m1 = Metric("Velocidade", [1,1,1,2,2,2,3,3,3,1,1])
+    m2 = Metric("Potencia")
+    m2.create_from_execution_list(list, 2)
+    print m2.get_executions_groups()
+    #m1.create_groups([1,1,1,2,2,2,3,3,3,1,1])
+
+
+    m2.show()
+    print m2.qtd_groups()
+    # m2.show()
+    # m3.show()
+
+main()
