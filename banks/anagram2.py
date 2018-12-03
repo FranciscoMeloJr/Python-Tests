a66fe55d91cc83af025c09e7d210bfaece1ceb41
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2018-12-03 01:59:54 -0500

    Anagram 2 and Anagram 3

---
+++ banks/anagram2.py
@@ -0,0 +1,45 @@
+# Based on insertion search
+
+def validate(words):
+    """
+    Compare the words in the list.
+    :param words: list of words
+    :return: new list of words.
+    """
+    result = {}
+    # Eliminate duplicate words:
+    words = list(set(words))
+    # Order:
+    words = sorted(words)
+
+    #Final result
+    final = []
+    # Get the size - create a dict: size:[word1,word2,word3]
+    if len(words) < 1:
+        return []
+    final.append(words[0])
+
+    flag = True
+    for i in range(1, len(words)):
+        flag = True
+        for each in final:
+            if set(words[i]) == set(each) and len(words[i]) == len(each):
+                flag = False
+        if flag:
+            final.append(words[i])
+    return final
+
+
+def test_assertions():
+    #validate:
+    assert validate(['ttoo','toto','xt']) == ['toto', 'xt']
+    assert validate(['ttoo','toto']) == ['toto']
+    assert validate(['ttoo','oott','toto']) == ['oott']
+    #size 2:
+    assert validate(['xt', 'tx']) == ['tx']
+    assert validate(['xt', 'tx', 'xx']) == ['tx', 'xx']
+    #empty
+    assert validate([]) == []
+
+
+test_assertions()
---
+++ banks/anagram3.py
@@ -0,0 +1,45 @@
+def validate(words):
+    """
+    Compare the words in the list and return the new string list.
+    :param words: list of words
+    :return: new list of words without repetition, i.e. toto == ttoo.
+    """
+    result = {}
+    # Eliminate duplicate words:
+    words = list(set(words))
+    # Order:
+    words = sorted(words)
+
+    #Final result
+    final = []
+    # Get the size - create a dict: size:[word1,word2,word3]
+    for each_word in words:
+        key = len(each_word)
+        if key not in result:
+            result[key] = [each_word]
+        else:
+            list_values = result[key]
+            # 2:[cra] - car
+            for each in list_values:
+                if set(each) != set(each_word):
+                    list_values.append(each_word)
+                    result[key] = list_values
+
+    #list comprehension for final list:
+    final = [result.get(each_key)[0] for each_key in result.keys()]
+    print(final)
+    return final
+
+
+def test_assertions():
+    """
+    Do the assertions.
+    """
+    #validate:
+    assert validate(['ttoo','toto','xt']) == ['toto', 'xt']
+    assert validate(['ttoo','toto']) == ['toto']
+    assert validate(['xt','tx']) == ['tx']
+    assert validate(['xyt','xty','ytx']) == ['xty']
+    assert validate(['']) == ['']
+
+test_assertions()
