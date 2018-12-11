ca869ee2ccbf71bf58f1fe2169f765f57c767379
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2018-12-11 03:03:25 -0500

    bad request

--- dogs-of-nyc-api/api.py
+++ dogs-of-nyc-api/api.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import requests
 
 app = Flask(__name__)
@@ -8,26 +8,34 @@ app = Flask(__name__)
 @app.route('/count', methods=['GET'])
 def get_count():
     """
-    Return the count result from a query.
+    Return the account.
     :return: The count result.
     """
     base_uri = 'https://www.googleapis.com/fusiontables/v2/query?sql='
+    # API_KEY Acess:
     api_key = '&key=AIzaSyDMULA71K9KaGN9wPZF6_3Hnm4qOcKoiN0'
     query_str = 'SELECT * FROM 1pKcxc8kzJbBVzLu_kgzoAMzqYhZyUhtScXjB0BQ'
+    # String:
     query_string = request.query_string
+
+    fields_in_case_of_failure = []
 
     if '&' not in query_string:
         request_key = [query_string.split('=')[0]]
         request_value = [query_string.split('=')[1]]
-        query_str += ' WHERE ' + str(request_key[0]) + ' CONTAINS IGNORING CASE \'' + str(request_value[0] + '\'')
+        query_str += ' WHERE ' + str(request_key[0]) + ' MATCHES \'' + str(request_value[0] + '\'')
+
+        fields_in_case_of_failure.append(requestKey)
     else:
         query_str = create_query_dict(query_string)
 
-    #Do the query
-    json_response = requests.get(base_uri + "SELECT * FROM 1pKcxc8kzJbBVzLu_kgzoAMzqYhZyUhtScXjB0BQ" + api_key, headers={'Content-Type':'application/json'})
-
+    try:
+        json_response = requests.get(base_uri + query_str + api_key, headers={'Content-Type':'application/json'})
+        rows_length = len(json_response.json()['rows'])
+    except:
+        return bad_request_unknown_fields(fields_in_case_of_failure)
     if rowsLenght > 0:
-            return jsonify({'count': rowsLenght})
+        return jsonify({'count': rows_length})
     return abort(400)
 
 
@@ -38,10 +46,10 @@ def create_query_dict(query_string):
     """
 
     request_data ={each.split('=')[0]: each.split('=')[1] for each in query_string.split('&')}
-    #iterating Over dict:
+    # iterating Over dict:
     for i, (requestKey, requestValue) in enumerate(request_data.items()):
-        print(i)
         if i == 0:
+            # Ignores case vs Match
             query_str += ' WHERE ' + requestKey + ' CONTAINS IGNORING CASE \'' + requestValue + "\'"
         else:
             if requestValue.isdigit():
@@ -50,6 +58,18 @@ def create_query_dict(query_string):
                 query_str += ' AND ' + requestKey + ' CONTAINS IGNORING CASE \'' + requestValue + "\'"
 
     return query_str
+
+
+def bad_request_unknown_fields(fields):
+    """
+    Returns unknown fields message request.
+    :return:  Response.
+    """
+    # Alphabetic order:
+    fields.sort()
+    response = jsonify({'unknown fields': fields})
+    response.status_code = 400
+    return response
 
 
 if __name__ == '__main__':
