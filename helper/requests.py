58d8bab4bf1d51179631ad7511eb26d0daa89832
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2019-03-03 04:21:45 -0500

    1- Create test directory. 2- Requests.py: parsing kcs. 3- Creation of solution.html for testing

--- helper/requests.py
+++ helper/requests.py
@@ -1,8 +1,64 @@
-def view_content(url="http://www.g1.globo.com"):
-    import urllib.request
-    import html2text
-    from bs4 import BeautifulSoup
+import sys
+import urllib.request
+import html2text
+from bs4 import BeautifulSoup
 
+#Get Title
+def get_title_kcs(url_kcs):
+    content = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(content, features="html.parser")
+    #soup = BeautifulSoup(open("test\solution.html"), "html.parser")
+    return soup.title.string
+
+#Get Content
+def get_content(url_kcs="test\solution.html"):
+    #content = urllib.request.urlopen(url).read()
+    #soup = BeautifulSoup(content, features="html.parser")
+    soup = BeautifulSoup(open("test\solution.html"), "html.parser")
+    return soup.text
+
+#Get Parsed Content
+def parse_kcs(url="http://www.g1.globo.com"):
+    dit_text = {}
+    #req = urllib.request.Request('http://www.g1.com.br')
+    #url = "http://www.g1.globo.com"
+    try:
+        soup = BeautifulSoup(open("test\solution.html"), "html.parser")
+        #Print all as text:
+        #print(soup.text)
+
+        #Print all:
+        #print(soup.prettify())
+
+        # Title:
+        dit_text['title'] = soup.title.string
+
+        # Environment:
+        dit_text['env'] = soup.find_all("section", class_="field_kcs_environment_txt")
+        html_str1 = soup.find_all("section", class_="field_kcs_environment_txt")
+        soup1 = BeautifulSoup(html_str1, features="html.parser)
+        print(soup1.get_text())
+        #html_str1 = soup.find_all("section", class_="field_kcs_environment_txt")
+        #soup1 = BeautifulSoup(html_str1, features="html.parser")
+        #dit_text['env'] = soup1.get_text()
+        #print(soup1.get_text())
+
+        # Issue:
+        dit_text['issue'] = soup.find_all("section", class_="field_kcs_issue_txt")
+
+        # Resolution:
+        dit_text['resolution'] = soup.find_all("section", class_="field_kcs_resolution_txt")
+
+        # Observation:
+        dit_text['observation'] = soup.find_all("section", class_="field_kcs_observation_txt")
+        return dit_text
+    except:
+        e = sys.exc_info()[0]
+        print(e)
+        print("Exception opening content in Beautifulsoup")
+
+#View Content
+def view_content(url="http://www.g1.globo.com"):
     #req = urllib.request.Request('http://www.g1.com.br')
     #url = "http://www.g1.globo.com"
     try:
@@ -16,9 +72,11 @@ def view_content(url="http://www.g1.globo.com"):
         print("Exception opening content in Beautifulsoup")
 
     #All info:
-    #print(soup.prettify())
+    print(soup.prettify())
 
     #Title:
     print(soup.title.string)
 
-view_content()
+
+dict_d = parse_kcs()
+#print(dict_d['env'])
