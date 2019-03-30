5b7ab7b63ac80fe27487919cfa741a7d715e03e8
Francisco de Melo jr <francisco_melo_jr@hotmail.com>
2019-03-30 19:16:01 -0400

    Create requestUnique.py

---
+++ jmap/requestUnique.py
@@ -0,0 +1,27 @@
+#Import for the libs:
+import urllib2
+import sys
+
+def do_request(arg):
+   #print sys.argv[1]
+   #Do the request:
+   some_url = 'http://localhost:8080/servlet/HelloServlet?buffer='+arg
+   #Get content
+   try:
+       content = urllib2.urlopen(some_url).read()
+       #Print content
+       return 1
+   except:
+       return -1
+
+def do_test(arg):
+   i = 0
+   while i < int(arg):
+       try:
+           do_request(str(i))
+       except:
+           print "it is down"
+           break
+       i+=1
+
+do_request(sys.argv[1])
